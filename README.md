5933187fe4a28528bca9c839e4a452689ca5e95a
Andi Hakim <andi.hakim.arif@gmail.com>
2019-01-12 14:06:26 +0700

    add storybook states

--- README.md
+++ README.md
@@ -1,16 +1,36 @@
 # belajar-storybook-taskbox
+
 sumbernya di https://www.learnstorybook.com
 
 # instalasi
+
 ## create react app
+
 ```bash
 yarn create react-app belajar-storybook-taskbox
 ```
+
 _yarn_ = package manager alternatif NPM, lebih cepat & outpuf file-nya lebih kecil daripada NPM
 
 ## storybook
+
 ```bash
 npx -p @storybook/cli sb init
 ```
+
 npx = pengganti NPM
 _-p_ = package
+
+# storybook
+
+## storiesOf()
+
+mendaftarkan _component_ ke storybook
+
+## action
+
+mendaftarkan _action_ / fungsi klik
+
+## add()
+
+menambahkan menu di sebelah kiri
--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 
-import Task from './Task';
+import Task from "./Task";
 
 export const task = {
-	id: '1',
-	title: 'Test Task',
-	state: 'TASK_INBOX',
-	updatedAt: new Date(2018, 0, 1, 9, 0),
+  id: "1",
+  title: "Test Task",
+  state: "TASK_INBOX",
+  updatedAt: new Date(2018, 0, 1, 9, 0)
 };
 
 export const actions = {
-	onPinTask: action('onPintask'),
-	onArchieveTask: action('onArchieveTask'),
+  onPinTask: action("onPintask"),
+  onArchieveTask: action("onArchiveTask")
 };
 
-storiesOf('Task', module)
-	.add('default', () => <Task task={task} {...actions} />)
-	.add('pinned', () => <Task task={{ ...task, state: 'TASK_PINNED'}} {...actions} />)
-	.add('archived', () => <Task task={{ ...task, state: 'TASK_ARCHIVED'}} {...actions} />);
+storiesOf("Task", module)
+  .add("default", () => <Task task={task} {...actions} />)
+  .add("pinned", () => (
+    <Task task={{ ...task, state: "TASK_PINNED" }} {...actions} />
+  ))
+  .add("archived", () => (
+    <Task task={{ ...task, state: "TASK_ARCHIVED" }} {...actions} />
+  ));
 
-	// https://www.learnstorybook.com/react/en/simple-component/
+// https://www.learnstorybook.com/react/en/simple-component/
